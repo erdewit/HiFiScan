99179cda45ab76d279d7d2b354e27b1061767082
Ewald de Wit <ewald.de.wit@gmail.com>
2023-03-11 18:11:22 +0100

    v1.4.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", 'r', encoding="utf-8") as f:
 
 setup(
     name='hifiscan',
-    version='1.4.2',
+    version='1.4.3',
     description='Optimize the audio quality of loudspeakers',
     long_description=long_description,
     packages=['hifiscan'],
